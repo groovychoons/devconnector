6609a6d588803f33e8796eb20144a4b2f51bda18
Zara Siddique <zara@siddique.eu>
2019-03-11 20:39:17 +1100

    Secured keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://admin:123321z@ds155825.mlab.com:55825/volconnect",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
